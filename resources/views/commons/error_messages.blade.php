b4452fcdc40f79aeb9e12b18d7f29ebeccae3baf
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-06-01 03:39:18 +0000

    done2

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
+
+    @if (count($errors) > 0)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+
+    {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('更新') !!}
+
+    {!! Form::close() !!}
+
+@endsection
