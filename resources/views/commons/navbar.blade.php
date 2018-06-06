75b9a0f52e683173c6306e9e649893fd8f30ddf3
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-06-06 04:56:43 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,15 +8,24 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Task Management</a>
+                <a class="navbar-brand" href="/">Meglytasks</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                    <li><a href="#">Login</a></li>
-                    <li>{!! link_to_route('tasks.create', 'Add New Task') !!}</li>
-                    <li>{!! link_to_route('tasks.store', 'Show My Tasks') !!}</li>
-                    <li>{!! link_to_route('tasks.create', 'Logout') !!}</li>
+                    @if (Auth::check())
+                        <li><a href="#">Users</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">My profile</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                        </li>
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login', 'Login') !!}</li>
+                    @endif
                 </ul>
             </div>
         </div>
